3eccd981b4ff5bcafd82f9cf69a42ab14e03e7a9
Oznur Celebi <o.ozgumus17@gmail.com>
2021-10-28 14:14:53 +0200

    js is completed

--- app.js
+++ app.js
@@ -3,19 +3,29 @@ let screen = document.querySelector(".calc-numbers");
 let calculator = dugme[18];
 let clear_button = dugme[0];
 let sign_changer = dugme[1];
-let result_screened = false;
+let delete_button =dugme[2];
+let result_displayed = false;
 
-if (result_screened = true) {
-
-}
+// element.addEventListener("click", print);
+// function print() {
+//     screen.value += element.innerHTML;
+// }
 dugme.forEach(element => {
-    if (element.innerHTML != "+/-" && element.innerHTML != "AC" && element.innerHTML != "&#129044" && element.innerHTML != "=") {
-        element.addEventListener("click", print);
-    }
-    function print() {
-        screen.value += element.innerHTML;
+    element.addEventListener("click", ()=>{
+    if (element.innerHTML != "+/-" && element.innerHTML != "AC" && element.innerHTML != "← " && element.innerHTML != "=") {
+        if (result_displayed === false) {
+            screen.value += element.innerHTML;
+        } else if (result_displayed === true && (element.innerHTML === "+" || element.innerHTML === "-" || element.innerHTML === "×" || element.innerHTML === "÷")){
+            result_displayed = false;
+            screen.value += element.innerHTML;
+        } else {
+            screen.value="";
+            result_displayed=false;
+           screen.value += element.innerHTML;
+        }
     }
 })
+});
 calculator.addEventListener("click", () => {
     let array = screen.value.split("");
     screen.value = calculate(array);
@@ -32,33 +42,33 @@ function calculate(array) {
         let y = array.slice(i + 1).join("");
         console.log(x);
         console.log(y);
-        result_screened = true;
-        console.log(result_screened);
+        result_displayed = true;
+        console.log(result_displayed);
         return divide(parseFloat(x), parseFloat(y))
     }
     if (array.includes("x") == true) {
         i = array.indexOf("x");
         let x = array.slice(0, i).join("");
         let y = array.slice(i + 1).join("");
-       result_screened = true;
-        console.log(result_screened);
+        result_displayed = true;
+        console.log(result_displayed);
         return mul(parseFloat(x), parseFloat(y));
     }
     if (array.includes("-") == true) {
         i = array.indexOf("-");
         let x = array.slice(0, i).join("");
         let y = array.slice(i + 1).join("");
-        result_screened = true;
-        console.log(result_screened);
+        result_displayed = true;
+        console.log(result_displayed);
         return sub(parseFloat(x), parseFloat(y));
-        
+
     }
     if (array.includes("+") == true) {
         let i = array.indexOf("+");
         let x = array.slice(0, i).join("");
         let y = array.slice(i + 1).join("");
-        result_screened = true;
-        console.log(result_screened);
+        result_displayed= true;
+        console.log(result_displayed);
         return sum(parseFloat(x), parseFloat(y));
     }
 
@@ -85,12 +95,15 @@ function sum(a, b) {
     return (a + b)
 }
 
-
-clear_button.addEventListener("click", clear);
-function clear() {
-    screen.value = "0";
+clear_button.onclick =()=>{
+    screen.value = "";
+};
+sign_changer.onclick=()=>{
+    screen.value = screen.value * (-1);
 }
-sign_changer.addEventListener("click", change);
-function change() {
-    screen.value = screen.value*(-1);
+delete_button.onclick=("click", del);
+function del(){
+    screen.value = screen.value.slice(0,-1);
 }
+// let value ="34+4";
+// console.log(value.slice(0,-1));
